340aa004cffcbf39303dea02270af95aa712b0c7
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-07-07 12:56:10 +0600

    added various server error handlers.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 const app = express();
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Tour Management System.");
+  res.send("🏞️ Welcome to Tour Management System.");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ let server: Server;
 const PORT = 5000;
 const startServer = async () => {
   await mongoose.connect("mongodb://localhost:27017");
-  console.log("Connected to DataBase.");
+  console.log("🗄️ Connected to DataBase.");
 
   server = app.listen(PORT, () => {
     try {
       console.log(
-        `TourSuite-Tour-Management-System is Listening to Port ${PORT}.`
+        `🏞️ TourSuite-Tour-Management-System is Listening to Port ${PORT}.`
       );
     } catch (error) {
       console.log(error);
@@ -19,3 +19,45 @@ const startServer = async () => {
 };
 
 startServer();
+
+// UnHandled Rejection Error //
+process.on("unhandledRejection", (err) => {
+  console.log("UnHandled Rejection Detected!...🔌Server Shutting Down🔌", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+// UnCaught Exception Error //
+process.on("uncaughtException", (err) => {
+  console.log("🔌UnCaught Exception Detected!...🔌Server Shutting Down🔌", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+//  Signal Termination Error //
+process.on("SIGTERM", () => {
+  console.log("🔌SIGTERM Signal Received!...🔌Server Shutting Down🔌");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//  Signal Termination (Server Shutting Down manually)//
+process.on("SIGINT", () => {
+  console.log("🔌Server Shutting Down Gracefully...🔌");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
