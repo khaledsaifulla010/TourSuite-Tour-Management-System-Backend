c394745605c2ea4894461dc1865c2b73bb6e3082
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-07-09 12:11:44 +0600

    create user controller & route with organize system

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import { router } from "./app/routes";
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+// API END POINTS
+app.use("/api/v1", router);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("🏞️ Welcome to Tour Management System.");
 });
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({
+      name,
+      email,
+    });
+    res.status(httpStatus.CREATED).json({
+      sucess: true,
+      message: "User Created Sucessfully.",
+      user,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: `Something Went Wrong!! ${error.message}`,
+      error: error,
+    });
+  }
+};
+export const UserControllers = {
+  createUser,
+};
